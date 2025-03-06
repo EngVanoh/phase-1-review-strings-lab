2e264272871382c1bae63a5194427354365c75f5
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-03-06 23:11:03 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser=`Evans Pkor`;
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting=`Welcome, ${currentUser.charAt(0)}!`
